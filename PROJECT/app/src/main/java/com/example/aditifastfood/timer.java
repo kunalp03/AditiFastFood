4471bfaa47616bb4111ecb3e6284a395303d91f8
Sameep Hedaoo <sameephedaoo123@gmail.com>
2023-03-29 13:47:34 +0530

    PAYMENT GATEWAY AND TIMER ACTIVITY UPDATED

--- PROJECT/app/src/main/java/com/example/aditifastfood/timer.java
+++ PROJECT/app/src/main/java/com/example/aditifastfood/timer.java
@@ -3,10 +3,15 @@ package com.example.aditifastfood;
         import androidx.annotation.RequiresApi;
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.annotation.SuppressLint;
+        import android.content.Intent;
         import android.os.Build;
         import android.os.Bundle;
 
         import android.app.Activity;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
         import android.widget.TimePicker;
         import android.widget.Toast;
 
@@ -23,7 +28,11 @@ public class timer extends AppCompatActivity {
 
         // Get a reference to the TimePicker widget
         timePicker = findViewById(R.id.timePicker);
-
+        Button finish = findViewById(R.id.button4);
+        Button homepage = findViewById(R.id.button6);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        TextView timeText = findViewById(R.id.textView68);
+        TextView ordernum = findViewById(R.id.textView67);
         // Set a listener for when the user changes the selected time
         timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
@@ -34,9 +43,25 @@ public class timer extends AppCompatActivity {
                 // get the current date and time
                 LocalDateTime currentDateTime = LocalDateTime.now();
                 String ordernumber = hourOfDay +""+ minute+""+currentDateTime;
+//                Toast.makeText(timer.this, "" + selectedTime, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(timer.this, "" + ordernumber, Toast.LENGTH_LONG).show();
 
-                Toast.makeText(timer.this, "Your order will be ready at : " + selectedTime, Toast.LENGTH_SHORT).show();
-                Toast.makeText(timer.this, "Your order number is " + ordernumber, Toast.LENGTH_LONG).show();
+                finish.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(timer.this, "Your order will be ready at : " + selectedTime, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(timer.this, "Your order number is: " + ordernumber, Toast.LENGTH_SHORT).show();
+                        timeText.setText("Order Time:"+selectedTime);
+                        ordernum.setText("Order Number:"+ordernumber);
+                    }
+                });
+                homepage.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(timer.this,Home.class);
+                        startActivity(intent);
+                    }
+                });
             }
      });
 }
